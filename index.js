fd4194a252659fc5c44bff2b3749554f328d09a2
The Peach Dev <thepeachdev@gmail.com>
2026-03-23 21:36:40 +0530

    Changes done

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const name = "Yugeshwar nikita";
-console.log("Hii,   learning with " + name);
+const name = "Nikita Yugeshwar";
+console.log("Hii,learning with " + name);
+
+console.log(
+  "This is my first git learning project, Nikita Mam please aap mujhe ye padha do nhi to ma kabhi sikh nhi paaunge , aapse pdhne me abhut maja aat ah mujhe, LY",
+);
