c59f07d3f2e04bee02c7c339e5cfe2fdb6e87dc4
The Peach Dev <thepeachdev@gmail.com>
2026-03-23 21:41:21 +0530

    new changs

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const name = "Nikita Yugeshwar";
 console.log("Hii,learning with " + name);
 
 console.log(
-  "This is my first git learning project, Nikita Mam please aap mujhe ye padha do nhi to ma kabhi sikh nhi paaunge , aapse pdhne me abhut maja aat ah mujhe, LY",
+  "This is my first git learning project, Nikita Mam please aap mujhe ye padha do nhi to ma kabhi sikh nhi paaunge , aapse pdhne me abhut maja aat ah mujhe,",
+);
+
+console.log(
+  "I am learning git and github, I am loving it, I am enjoying it, I am having fun with it, I am loving it, I am enjoying it, I am having fun with it, I am loving it, I am enjoying it, I am having fun with it, I am loving it, I am enjoying it, I am having fun with it,",
 );
