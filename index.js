5b13124af23ed87e75c0d99ff679737b9c41201d
nikitayugeshwar <nikitayugeshwar01@gmail.com>
2026-03-23 21:41:43 +0530

    final commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const name = "Nikita Yugeshwar";
 console.log("Hii,learning with " + name);
 
 console.log(
-  "This is my first git learning project, Nikita Mam please aap mujhe ye padha do nhi to ma kabhi sikh nhi paaunge , aapse pdhne me abhut maja aat ah mujhe, LY",
+  "This is my first git learning project, Nikita Mam please aap mujhe ye padha do nhi to ma kabhi sikh nhi paaunge , aapse pdhne me abhut maja aat ah mujhe",
 );
